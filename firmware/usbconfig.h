4276c4d6bc854b3aa7fb47549be683b68f031600
Garret Jennings <gjennings@me.com>
2017-06-19 15:44:42 -0700

    Include common model name in descriptor.

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -252,8 +252,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'S', 't', 'e', 'p', '-', 't', 'o', '-', 'T', 'a', 'l', 'k'
-#define USB_CFG_DEVICE_NAME_LEN 12
+#define USB_CFG_DEVICE_NAME     'S', 't', 'e', 'p', '-', 't', 'o', '-', 'T', 'a', 'l', 'k', ' ', 'S', 'T', '0', '1'
+#define USB_CFG_DEVICE_NAME_LEN 17
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
